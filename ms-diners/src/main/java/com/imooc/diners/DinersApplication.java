6d90f7e09f0eca897407f46c05b5bb22a76867ef
xiejiahao33 <jhxie33@gmail.com>
2023-06-07 00:02:16 +0800

    Initial commit

---
+++ ms-diners/src/main/java/com/imooc/diners/DinersApplication.java
@@ -0,0 +1,14 @@
+package com.imooc.diners;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinersApplication.class, args);
+    }
+
+}
